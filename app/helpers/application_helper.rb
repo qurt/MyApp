dd03ab2694f132ec129487605ffe607cd3a74b6e
qurt <q.rainheart@gmail.com>
2013-07-24 16:02:07 +0400

    Add name to users model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def current_user
     user = User.find_by_id(session[:user_id])
-    user_name = user.login
+    user_name = user.name
     user_name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130717060537) do
+ActiveRecord::Schema.define(:version => 20130724113925) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20130717060537) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
