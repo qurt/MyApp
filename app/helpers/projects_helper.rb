47bf2d5c98f36786076ab000b20d264bc6fdecfc
qurt <q.rainheart@gmail.com>
2013-08-06 22:18:05 +0400

    Подсчет выполненых заданий в проекте

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+	def TaskCount(project_id)
+		count = Project.find_by_id(project_id).tasks.count
+		count
+	end
+	def inactiveTaskCount(project_id)
+		count = Project.find_by_id(project_id).tasks.find_all_by_active(false).count
+		count
+	end
 end
