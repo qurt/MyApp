593414c0a70966f5b13361be62ee9aac5f3d6fcf
qurt <q.rainheart@gmail.com>
2013-08-06 06:25:42 +0000

    .

--- db/schema.rb
+++ db/schema.rb
@@ -61,10 +61,10 @@ ActiveRecord::Schema.define(:version => 20130804201650) do
   create_table "users", :force => true do |t|
     t.string   "login"
     t.string   "password_digest"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "name"
-    t.boolean  "superuser",       :default => true
+    t.boolean  "superuser",       :default => false
   end
 
 end
