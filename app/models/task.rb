490695c877cfbc4e641468261b72743852f7493a
qurt <q.rainheart@gmail.com>
2013-08-08 17:50:55 +0000

    Some fix in Task page style

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :active, :content, :owner_id, :performer_id, :project_id, :title
   belongs_to :project
   belongs_to :stage
+  belongs_to :user, foreign_key: "performer_id"
   has_many :subtasks, dependent: :destroy, autosave: true
   has_many :discussions, dependent: :destroy 
 end
