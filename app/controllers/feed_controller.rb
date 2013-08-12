e38cc90f3eb92d944283ac6d77f87fcdad362d62
qurt <q.rainheart@gmail.com>
2013-08-12 17:31:21 +0400

    Change list item display style

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,7 +1,7 @@
 class FeedController < ApplicationController
   def index
 
-    tasks = Task.select("id, date(deadline) as deadline, title").where(performer_id: 1, active: true).order('deadline DESC')
+    tasks = Task.select("id, date(deadline) as deadline, title, project_id").where(performer_id: 1, active: true).order('deadline DESC')
     list_today = tasks.select{|item| item.deadline == Date.today}
     list_tomorrow = tasks.select{|item| item.deadline == Date.tomorrow}
     list_other = tasks.select{|item| item.deadline > Date.tomorrow}
