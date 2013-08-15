0d24ce5d02bc5f4ec4015c4ea9ac949b0d23254e
qurt <q.rainheart@gmail.com>
2013-08-15 19:39:31 +0400

    Add project_id to groups

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :owner_id
   has_many :tasks, dependent: :destroy
   has_many :stages, dependent: :destroy
+  has_many :groups, dependent: :destroy
   has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815152144) do
+ActiveRecord::Schema.define(:version => 20130815153745) do
 
   create_table "discussions", :force => true do |t|
     t.integer  "user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130815152144) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "project_id"
   end
 
   create_table "projects", :force => true do |t|
