5f13c759a578f35fcaf79ff7bb4134d932ecb4d2
qurt <q.rainheart@gmail.com>
2013-09-25 19:32:09 +0400

    Удалено лишнее

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-	$('.btn-success').click(function() {
-		$('.subtask_item_box').append('<div class="subtask_item"><input id="subtask_title_" class="form-control" name="subtask_title[]" type="text"></div><div style="clear: both"></div>');
-	});
-	$('.btn-danger').click(function() {
-		$(this).parent().remove();
-	});
-
 	$('#subtask').change(function() {
 		var post = null;
 		var val_id = $(this).attr('value');
