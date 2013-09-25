a522861be141e334a7b18743e226ceeb4cdfe566
qurt <q.rainheart@gmail.com>
2013-09-25 16:35:43 +0400

    Удаляем задачи при удалении стадии проекта

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,7 +1,7 @@
 class Stage < ActiveRecord::Base
   attr_accessible :deadline, :project_id, :title
 
-  has_many :tasks
+  has_many :tasks, depentent: :destroy
   has_many :group, dependent: :destroy
   belongs_to :project
 end
