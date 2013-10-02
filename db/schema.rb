f0a903617c345fb39d4deca91c5a77d3b4c3149f
Qurt <q.rainheart@gmail.com>
2013-10-02 23:34:21 +0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -71,11 +71,6 @@ ActiveRecord::Schema.define(:version => 20130815153745) do
     t.integer  "group_id",                    :default => 0
   end
 
-  create_table "tasks_users", :id => false, :force => true do |t|
-    t.integer "user_id"
-    t.integer "task_id"
-  end
-
   create_table "users", :force => true do |t|
     t.string   "login"
     t.string   "password_digest"
